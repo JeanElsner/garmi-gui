aa8e41d15f8dbc3ba2acfa70b6b24945ee5d0f65
JeanElsner <jean.elsner@googlemail.com>
2024-07-06 16:28:08 +0200

    fix: catch pygame exception for graceful exit This fixes a timing issue where a call to flip the pygame screen may happen even after pygame was quit preventing a graceful exit.

--- src/garmi_gui/gui.py
+++ src/garmi_gui/gui.py
@@ -157,14 +157,17 @@ class GUI:
             return
 
         while self.running:
-            for event in pygame.event.get():
-                if (
-                    event.type == pygame.QUIT
-                    or event.type == pygame.KEYDOWN
-                    and event.key == pygame.K_ESCAPE
-                ):
-                    self.running = False
-            pygame.display.flip()
+            try:
+                for event in pygame.event.get():
+                    if (
+                        event.type == pygame.QUIT
+                        or event.type == pygame.KEYDOWN
+                        and event.key == pygame.K_ESCAPE
+                    ):
+                        self.running = False
+                pygame.display.flip()
+            except pygame.error:
+                self.running = False
 
             self.clock.tick(10)  # Throttle loop to reduce CPU usage
 
